c515ec21473cefdc6bd94b66e348acc1210bfff0
Travis Scoville <travisscoville@gmail.com>
2024-06-05 15:27:30 -0600

    Completed learning activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Bob Jones", "Geology");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math = new("Jeremy Stones", "Factoring", "5.3", "4-14");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new("Sarah Robertson", "Music Theory", "Properties of Tritones");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
